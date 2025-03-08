ea1822d75cc55763fc2c27a8fb1340e92ff113ce
Sinchana2004-gif <vvce22ec0067@vvce.ac.in>
2025-03-08 23:04:24 +0530

    Add files via upload

---
+++ day2up/main.c
@@ -0,0 +1,10 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include "hello.h"
+//
+//int main()
+//{
+//    printf("Hello world!\n");
+//    printMessage("Hello from hello.c with a parameter!");
+//    return 0;
+//}
